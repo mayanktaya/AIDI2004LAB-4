b6bc71e5f98b0a4f62bdeaaa2b309020a90baddd
mayanktaya <130588533+mayanktaya@users.noreply.github.com>
2024-07-18 00:14:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-import pandas as pd
-import numpy as np
-
-df=pd.read_csv('fish.csv')
-df
-
-# Independent and Dependent features
-X=df.iloc[:,:-1]
-y=df.iloc[:,-1]
-
-# Train Test Split
-from sklearn.model_selection import train_test_split
-
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
-
-# Implement Random Forest classifier
-from sklearn.ensemble import RandomForestClassifier
-classifier=RandomForestClassifier()
-classifier.fit(X_train,y_train)
-
-# Prediction
-y_pred=classifier.predict(X_test)
-
-# Check Accuracy
-from sklearn.metrics import accuracy_score
-score=accuracy_score(y_test,y_pred)
-score
-
 import numpy as np
 import pickle
 import pandas as pd
@@ -33,6 +5,8 @@ from flask import Flask, request
 from flask import Flask, request, jsonify, render_template
 
 app=Flask(__name__)
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
 
 @app.route('/')
 def home():
